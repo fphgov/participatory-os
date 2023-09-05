70439683012c4235031ac21c549a9276453293b7
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-09-05 10:51:57 +0200

    Moved JSON to csv the stat
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/App/src/Handler/Vote/StatisticsHandler.php
+++ src/App/src/Handler/Vote/StatisticsHandler.php
@@ -4,33 +4,32 @@ declare(strict_types=1);
 
 namespace App\Handler\Vote;
 
-use App\Entity\Campaign;
-use App\Entity\OfflineVote;
-use App\Entity\Phase;
-use App\Entity\Project;
-use App\Entity\Vote;
+use App\Model\VoteExportModel;
 use App\Service\PhaseServiceInterface;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Query\Expr\Join;
-use Laminas\Diactoros\Response\JsonResponse;
+use Laminas\Diactoros\Stream;
+use Laminas\Diactoros\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+use function ob_start;
+use function ob_get_clean;
+use function fopen;
+use function fputcsv;
+use function rewind;
+use function strval;
+
 final class StatisticsHandler implements RequestHandlerInterface
 {
-    /** @var EntityManager */
-    protected $em;
-
-    /** @var PhaseServiceInterface */
-    protected $phaseService;
-
     public function __construct(
-        EntityManager $em,
-        PhaseServiceInterface $phaseService
+        private EntityManager $em,
+        private PhaseServiceInterface $phaseService,
+        private VoteExportModel $voteExportModel,
     ) {
-        $this->em           = $em;
-        $this->phaseService = $phaseService;
+        $this->em              = $em;
+        $this->phaseService    = $phaseService;
+        $this->voteExportModel = $voteExportModel;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -38,87 +37,30 @@ final class StatisticsHandler implements RequestHandlerInterface
         $phase = $this->phaseService->getCurrentPhase();
 
         $queryParams   = $request->getQueryParams();
-        $campaignParam = $queryParams['campaign'] ?? '';
-        $detailParam   = $queryParams['detail'] ?? false;
 
-        $campaign = $phase->getCampaign();
+        $exportData = $this->voteExportModel->getCsvData($phase, $queryParams);
 
-        if ($campaignParam) {
-            $campaignRepository = $this->em->getRepository(Campaign::class);
+        ob_start();
+        ob_get_clean();
 
-            $campaign = $campaignRepository->find($campaignParam);
+        $stream = fopen('php://memory', 'wb+');
+
+        foreach ($exportData as $fields) {
+            fputcsv($stream, $fields, ";");
         }
 
-        $onlineResult  = $this->getOnlineVoteList($campaign, (bool)$detailParam);
-        $offlineResult = $this->getOfflineVoteList($campaign, (bool)$detailParam);
+        rewind($stream);
 
-        return new JsonResponse([
-            'data' => [
-                'online'  => $onlineResult,
-                'offline' => $offlineResult
-            ],
+        $body = new Stream($stream);
+
+        return new Response($body, 200, [
+            'Content-Type'              => 'text/csv; charset=utf-8',
+            'Content-Disposition'       => "attachment; filename=\"votes.csv\"",
+            'Content-Description'       => 'File Transfer',
+            'Pragma'                    => 'public',
+            'Expires'                   => '0',
+            'Cache-Control'             => 'must-revalidate',
+            'Content-Length'            => strval($body->getSize()),
         ]);
-    }
-
-    private function getOnlineVoteList(Campaign $campaign, ?bool $detail = null): array
-    {
-        $voteRepository = $this->em->getRepository(Vote::class);
-
-        $qb = $voteRepository->createQueryBuilder('v')
-            ->join(Project::class, 'p', Join::WITH, 'p.id = v.project')
-            ->join(Campaign::class, 'c', Join::WITH, 'c.id = p.campaign')
-            ->where('p.campaign = :campaign')
-            ->orderBy('p.id', 'DESC')
-            ->setParameters([
-                'campaign' => $campaign,
-            ]);
-
-        $votes = $qb->getQuery()->getResult();
-
-        return $this->normalizeVote($votes, $detail);
-    }
-
-    private function getOfflineVoteList(Campaign $campaign, ?bool $detail = null): array
-    {
-        $offlineVoteRepository = $this->em->getRepository(OfflineVote::class);
-
-        $qb = $offlineVoteRepository->createQueryBuilder('v')
-            ->join(Project::class, 'p', Join::WITH, 'p.id = v.project')
-            ->join(Campaign::class, 'c', Join::WITH, 'c.id = p.campaign')
-            ->where('p.campaign = :campaign')
-            ->orderBy('p.id', 'DESC')
-            ->setParameters([
-                'campaign' => $campaign,
-            ]);
-
-        $votes = $qb->getQuery()->getResult();
-
-        return $this->normalizeVote($votes, $detail);
-    }
-
-    private function normalizeVote(array $votes, ?bool $detail = null): array
-    {
-        $normalizedVotes = [];
-        foreach ($votes as $vote) {
-            $normVote = [
-                'id'      => $vote->getId(),
-                'user'    => $vote->getUser()->getId(),
-                'project' => $vote->getProject()->getId(),
-                'type'    => $vote->getVoteType()->getId(),
-                'created' => $vote->getCreatedAt()->format('Y-m-d H:i:s'),
-            ];
-
-            if ($detail) {
-                $normVote['project'] = [
-                    'id'    => $vote->getProject()->getId(),
-                    'title' => $vote->getProject()->getTitle(),
-                    'price' => $vote->getProject()->getCost(),
-                ];
-            }
-
-            $normalizedVotes[] = $normVote;
-        }
-
-        return $normalizedVotes;
     }
 }
--- src/App/src/Handler/Vote/StatisticsHandlerFactory.php
+++ src/App/src/Handler/Vote/StatisticsHandlerFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Handler\Vote;
 
+use App\Model\VoteExportModel;
 use App\Service\PhaseServiceInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
@@ -14,7 +15,8 @@ final class StatisticsHandlerFactory
     {
         return new StatisticsHandler(
             $container->get(EntityManagerInterface::class),
-            $container->get(PhaseServiceInterface::class)
+            $container->get(PhaseServiceInterface::class),
+            $container->get(VoteExportModel::class)
         );
     }
 }
