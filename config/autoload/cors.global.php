6ba8f459702d3793728f40e91487517abbdbedf0
Robert Binder <binder.robert92@gmail.com>
2024-11-04 12:33:48 +0100

    16437

--- config/autoload/cors.global.php
+++ config/autoload/cors.global.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 use Mezzio\Cors\Configuration\ConfigurationInterface;
 
-$corsOrigins = explode(',', getenv('CORS_ORIGIN'));
+const ALLOWED_HEADERS = [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authorization'
+];
+
+$corsOrigins = explode(',', getenv('CORS_ORIGIN') ?: '');
 
 return [
     ConfigurationInterface::CONFIGURATION_IDENTIFIER => [
-        'allowed_origins' => is_array($corsOrigins) ? $corsOrigins : [],
-        'allowed_headers' => ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+        'allowed_origins' => $corsOrigins ?: [],
+        'allowed_headers' => [...ALLOWED_HEADERS],
         'allowed_max_age' => '3600',
         'credentials_allowed' => false,
         'exposed_headers' => [],
