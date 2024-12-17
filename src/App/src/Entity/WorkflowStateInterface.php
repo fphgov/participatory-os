cdcc8792af9ad3c2c47a9814bf54a135686821a7
Horváth Tamás András <htomy92@gmail.com>
2024-12-17 18:40:25 +0100

    New workflow states
    Signed-off-by: Horváth Tamás András <htomy92@gmail.com>

--- src/App/src/Entity/WorkflowStateInterface.php
+++ src/App/src/Entity/WorkflowStateInterface.php
@@ -6,19 +6,23 @@ namespace App\Entity;
 
 interface WorkflowStateInterface
 {
-    public const STATUS_RECEIVED           = 100;
-    public const STATUS_PUBLISHED          = 110;
-    public const STATUS_PUBLISHED_WITH_MOD = 111;
-    public const STATUS_PRE_COUNCIL        = 120;
-    public const STATUS_PROF_DONE          = 121;
-    public const STATUS_VOTING_LIST        = 130;
-    public const STATUS_UNDER_CONSTRUCTION = 140;
-    public const STATUS_READY              = 200;
-    public const STATUS_STATUS_REJECTED    = 510;
-    public const STATUS_COUNCIL_REJECTED   = 530;
-    public const STATUS_NOT_VOTED          = 540;
-    public const STATUS_USER_DELETED       = 600;
-    public const STATUS_TRASH              = 610;
+    public const STATUS_RECEIVED                = 100;
+    public const STATUS_PUBLISHED               = 110;
+    public const STATUS_PUBLISHED_WITH_MOD      = 111;
+    public const STATUS_WAIT_FOR_PUBLIC_SUPPORT = 112;
+    public const STATUS_UNDER_EVALUATION        = 113;
+    public const STATUS_PRE_COUNCIL             = 120;
+    public const STATUS_PROF_DONE               = 121;
+    public const STATUS_VOTING_LIST             = 130;
+    public const STATUS_UNDER_CONSTRUCTION      = 140;
+    public const STATUS_READY                   = 200;
+    public const STATUS_STATUS_REJECTED         = 510;
+    public const STATUS_NO_PUBLIC_SUPPORT       = 511;
+    public const STATUS_NO_ENOUGH_SUPPORT       = 512;
+    public const STATUS_COUNCIL_REJECTED        = 530;
+    public const STATUS_NOT_VOTED               = 540;
+    public const STATUS_USER_DELETED            = 600;
+    public const STATUS_TRASH                   = 610;
 
     public const DISABLE_SHOW_DEFAULT = [];
 
